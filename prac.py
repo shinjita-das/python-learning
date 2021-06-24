a2eecaa0a5f6e85f45d2d0c7a529de55e0a0f8e7
Abhisaran <30199540+Abhisaran@users.noreply.github.com>
2021-06-24 19:53:27 +1000

    task2

--- prac.py
+++ prac.py
@@ -1,22 +1,36 @@
+def calc(input1, input2, operator):
+    value = None
+    # Start here
+    # if else
+    # math operators
+    # how to compare strings in python
+    if operator == "+":
+        value = input1 + input2
+    elif operator == "-":
+        value = input1 - input2
+    elif operator == "*":
+        value = input1 * input2
+    elif operator == "/":
+        value = input1 / input2
+    else:
+        print("Invalid operator")
+        # End here
+    return value
 
-def calc(input1, input2,operator):
-       value = None
-       if operator == "+":
-              value= number1+number2
-       elif operator == "-":
-              value= number1-number2
-       elif operator == "*":
-              value = number1 ** number2
-       elif operator == "/":
-              value = number1 // number2
-       else: print ("Inva lid")
 
-              return value
+def print_this(name):
+    print('Hey ' + name)
+    # return 0
 
-print("Enter your first value")
-number1 = int(input())
-print("Enter your second value")
-number2 = int(input())
-print("Enter you operator")
-op= input()
-print(calc(number1, number2,op))
+
+option = 1
+while option != 'exit':
+    print("Enter your first value")
+    number1 = int(input())
+    print("Enter your second value")
+    number2 = int(input())
+    print("Enter you operator")
+    op = input()
+    print(calc(number1, number2, op))
+    # print_this("Mat")
+    option = input("Type exit if you want to exit:")
