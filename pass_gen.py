988df881647edd3bd498785d661a505b275f7b5d
Abhisaran <30199540+Abhisaran@users.noreply.github.com>
2021-07-06 21:00:42 +1000

    new problems

---
+++ pass_gen.py
@@ -0,0 +1,5 @@
+#Generate a random password
+#Input: Ask user for number of characters:
+#Output: Generate a string with random numbers and letters and special characters
+#Example: Enter the number of characters: 10
+#Password: !Uy91*@76#
---
+++ strob numbers.py
@@ -0,0 +1,5 @@
+17. Write a Python program to get all strobogrammatic numbers that are of length n. Go to the editor
+A strobogrammatic number is a number whose numeral is rotationally symmetric, so that it appears the same when rotated 180 degrees. In other words, the numeral looks the same right-side up and upside down (e.g., 69, 96, 1001).
+For example,
+Given n = 2, return ["11", "69", "88", "96"].
+Given n = 3, return ['818', '111', '916', '619', '808', '101', '906', '609', '888', '181', '986', '689']
